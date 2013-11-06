594eb82724244314e997d5a4aa88efba268514d5
Eric Brelsford <ebrelsford@gmail.com>
2013-11-06 11:56:48 -0500

    No more django.conf.urls.defaults

--- livinglotsnola/organize/urls.py
+++ livinglotsnola/organize/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from livinglots import get_organizer_model
 from livinglots_organize.forms import OrganizerForm
