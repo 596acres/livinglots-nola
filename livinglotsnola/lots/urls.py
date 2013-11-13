5d7f022e71729bac60057bd1a7817c27774578dc
Eric Brelsford <ebrelsford@gmail.com>
2013-11-13 15:26:04 -0500

    Acres on map and lots' pages

--- livinglotsnola/lots/urls.py
+++ livinglotsnola/lots/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, url
 
 import livinglots_lots.urls as llurls
 
-from .views import LotsGeoJSONCentroid
+from .views import LotsCountViewWithAcres, LotsGeoJSONCentroid
 
 
 urlpatterns = patterns('',
 
     url(r'^geojson-centroid/', LotsGeoJSONCentroid.as_view(),
         name='nola_lot_geojson_centroid'),
+    url(r'^count/', LotsCountViewWithAcres.as_view(), name='lot_count'),
 
 ) + llurls.urlpatterns
--- livinglotsnola/lots/views.py
+++ livinglotsnola/lots/views.py
@@ -1,11 +1,23 @@
 import geojson
 import json
+from pint import UnitRegistry
+
+from django.db.models import Sum
 
 from inplace.views import GeoJSONListView
-from livinglots_lots.views import FilteredLotsMixin
+from livinglots_lots.views import FilteredLotsMixin, LotsCountView
+
+
+ureg = UnitRegistry()
 
 
 class LotGeoJSONMixin(object):
+
+    def get_acres(self, lot):
+        acres = getattr(lot, 'area_acres', None)
+        if not acres:
+            return 'unknown'
+        return round(acres, 2)
 
     def get_feature(self, lot):
         if lot.known_use:
@@ -34,7 +46,7 @@ class LotGeoJSONMixin(object):
                 'layer': layer,
                 'owner': str(lot.owner) or 'unknown',
                 'pk': lot.pk,
-                'size': getattr(lot, 'area', 'unknown'),
+                'size': self.get_acres(lot),
             },
         )
 
@@ -46,3 +58,29 @@ class LotsGeoJSONCentroid(LotGeoJSONMixin, FilteredLotsMixin, GeoJSONListView):
             field_name='centroid',
             precision=8,
         ).select_related('known_use', 'owner__owner_type')
+
+
+class LotsCountViewWithAcres(LotsCountView):
+
+    def get_area_in_acres(self, lots_qs):
+        sqft = lots_qs.aggregate(total_area=Sum('polygon_area'))['total_area']
+        if not sqft:
+            return 0
+        sqft = sqft * (ureg.feet ** 2)
+        acres = sqft.to(ureg.acre).magnitude
+        return int(round(acres))
+
+    def get_context_data(self, **kwargs):
+        lots = self.get_lots().qs
+        no_known_use = lots.filter(known_use__isnull=True)
+        in_use = lots.filter(known_use__isnull=False, known_use__visible=True)
+
+        context = {
+            'lots-count': lots.count(),
+            'lots-acres': self.get_area_in_acres(lots),
+            'no-known-use-count': no_known_use.count(),
+            'no-known-use-acres': self.get_area_in_acres(no_known_use),
+            'in-use-count': in_use.count(),
+            'in-use-acres': self.get_area_in_acres(in_use),
+        }
+        return context
