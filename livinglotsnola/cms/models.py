3321ff5da5a3a796192827b339d67fadb1700de1
Eric Brelsford <ebrelsford@gmail.com>
2013-11-16 17:23:20 -0500

    ExternallyLinkedMediaFile

--- livinglotsnola/cms/models.py
+++ livinglotsnola/cms/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
+from feincms.module.medialibrary.fields import ContentWithMediaFile
 from feincms.module.page.models import Page
 from feincms.content.application.models import ApplicationContent
-from feincms.content.richtext.models import RichTextContent
 from feincms.content.medialibrary.models import MediaFileContent
+from feincms.content.richtext.models import RichTextContent
 
 
 class CollapsibleSectionContent(RichTextContent):
@@ -34,6 +35,27 @@ class RecentActivitiesContent(models.Model):
         return render_to_string([
             'activity/plugin.html',
         ], {}, context_instance=kwargs.get('context'))
+
+
+class ExternallyLinkedMediaFileContent(ContentWithMediaFile):
+
+    class Meta:
+        abstract = True
+        verbose_name = _('externally linked media file')
+        verbose_name_plural = _('externally linked media files')
+
+    @classmethod
+    def initialize_type(cls):
+        cls.add_to_class('external_url', models.URLField(_('external url'),
+                                                         blank=True, null=True))
+
+    def render(self, **kwargs):
+        ctx = { 'content': self }
+        ctx.update(kwargs)
+        return render_to_string([
+            'content/externallylinkedmediafile/%s.html' % self.mediafile.type,
+            'content/externallylinkedmediafile/default.html',
+        ], ctx, context_instance=kwargs.get('context'))
 
 
 Page.register_extensions(
@@ -76,6 +98,8 @@ Page.create_content_type(RecentActivitiesContent)
 Page.create_content_type(MediaFileContent, TYPE_CHOICES=(
     ('default', _('default')),
 ))
+
+Page.create_content_type(ExternallyLinkedMediaFileContent)
 
 Page.create_content_type(ApplicationContent, APPLICATIONS=(
     ('livinglots_lots.map_urls', _('Lots map')),
