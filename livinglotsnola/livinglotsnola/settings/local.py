39bd7dd93c42bb351eefd76b0640a98295a91a52
Eric Brelsford <ebrelsford@gmail.com>
2013-12-22 23:48:41 -0500

    django-debug-toolbar==1.0

--- livinglotsnola/livinglotsnola/settings/local.py
+++ livinglotsnola/livinglotsnola/settings/local.py
@@ -50,25 +50,13 @@ LOGGING = {
 INTERNAL_IPS = ('127.0.0.1',)
 
 MIDDLEWARE_CLASSES += (
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
-    #'django_pdb.middleware.PdbMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django_pdb.middleware.PdbMiddleware',
 )
 
 INSTALLED_APPS += (
-    #'debug_toolbar',
-    #'django_pdb',
-)
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar',
+    'django_pdb',
 )
 
 DEBUG_TOOLBAR_CONFIG = {
