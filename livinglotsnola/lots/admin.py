0c238b955523cf2a7da974366ea30b75ef4ad893
Eric Brelsford <ebrelsford@gmail.com>
2014-01-22 15:54:17 -0500

    Extra lot admin fields

--- livinglotsnola/lots/admin.py
+++ livinglotsnola/lots/admin.py
@@ -1,1 +1,17 @@
-# Admins here
+from django.contrib import admin
+
+from livinglots_lots.admin import BaseLotAdmin
+
+from .models import Lot
+
+
+class LotAdmin(BaseLotAdmin):
+    fieldsets = BaseLotAdmin.fieldsets + (
+        ('Ownership', {
+            'fields': ('owner',),
+        }),
+    )
+
+
+admin.site.unregister(Lot)
+admin.site.register(Lot, LotAdmin)
